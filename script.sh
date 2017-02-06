c4f81b74132aaa4fafc32fc393b2360a26ddb28d
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2017-02-06 16:02:41 -0600

    remove git repo upon script completion

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 
-function newID {
+function newVersion {
 	NAME=`jq '.name' $1 | tr -d '"'`
 	PREV_ID=`apps-list -n $NAME | head -n1 | awk '{print $1;}'`
 	let LAST_CHAR="(${PREV_ID: -1} + 1) % 10"
@@ -7,7 +7,6 @@ function newID {
 	NEW_VERSION="${PREV_VERSION:0:-1}$LAST_CHAR"
 	echo $NEW_VERSION
 }
-
 
 WEBHOOK="${1}"
 CLONE_URL=`jq '.repository.clone_url' $WEBHOOK | tr -d '"'`
@@ -20,12 +19,13 @@ CHECK_TAGS=`basename $(dirname $REF)`			# ref/TAGS/branch
 CHECK_CREATED=`jq '.created' $WEBHOOK | tr -d '"'`
 
 IS_RELEASE=false
-IS_VALID_COMMIT=false
+#IS_VALID_COMMIT=false
 if [ "$CHECK_TAGS" == "tags" ]; then
 	IS_RELEASE=true
-elif [ "$CHECK_BRANCH" == "master" ] || [ $CHECK_CREATED = false ]; then
-	IS_VALID_COMMIT=true
-else
+#elif [ "$CHECK_BRANCH" == "master" ] || [ $CHECK_CREATED = false ]; then
+#	IS_VALID_COMMIT=true
+#else
+elif ! [ "$CHECK_BRANCH" == "master" ] && ! [ $CHECK_CREATED = false ]; then
 	echo "This is not a simple commit or release. Exiting without updating app."
 	exit
 fi
@@ -40,20 +40,23 @@ if ! [ -e "$DESCRIPTION_FILE" ]; then
 fi
 
 # set up version
-VERSION=`jq '.version' $DESCRIPTION_FILE | tr -d '"'`
-if [[ "$VERSION" == "(sourceref)" ]]; then
+PREV_VERSION=`jq '.version' $DESCRIPTION_FILE | tr -d '"'`
+if [[ "$PREV_VERSION" == "(sourceref)" ]]; then
 	if [ $IS_RELEASE = true ]; then
 		REPLACEMENT=`basename $REF`
 	else
-		REPLACEMENT=`newID $DESCRIPTION_FILE`
+		REPLACEMENT=`newVersion $DESCRIPTION_FILE`
 	fi
 
 	# update description file
-        VERSION="${VERSION/(sourceref)/$REPLACEMENT}"
-	CHANGE_DESCRIPTION_FILE=`jq --arg foo $VERSION '.version = $foo' $DESCRIPTION_FILE`
+        NEW_VERSION="${PREV_VERSION/(sourceref)/$REPLACEMENT}"
+	CHANGE_DESCRIPTION_FILE=`jq --arg foo $NEW_VERSION '.version = $foo' $DESCRIPTION_FILE`
 	rm $DESCRIPTION_FILE
 	echo $CHANGE_DESCRIPTION_FILE >> $DESCRIPTION_FILE
 fi
 
 # register app
 apps-addupdate -F $DESCRIPTION_FILE
+
+# remove git repo
+rm -rf $REPO_NAME
